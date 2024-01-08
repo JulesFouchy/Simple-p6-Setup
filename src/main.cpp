3724f419da2ef3c50aced44aa9c0c378a45850d4
Jules Fouchy <jules.fouchy@ntymail.com>
2024-01-08 22:23:54 +0100

    🔥 Remove CI

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
 #include <cstdlib>
-#include "p6/p6.h"
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
+#include "p6/p6.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    { // Run the tests
-        if (doctest::Context{}.run() != 0)
-            return EXIT_FAILURE;
-        // The CI does not have a GPU so it cannot run the rest of the code.
-        const bool no_gpu_available = argc >= 2 && strcmp(argv[1], "-nogpu") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        if (no_gpu_available)
-            return EXIT_SUCCESS;
-    }
+    // Run the tests
+    if (doctest::Context{}.run() != 0)
+        return EXIT_FAILURE;
 
-    // Actual app
+    // Actual application code
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
     ctx.maximize_window();
 
